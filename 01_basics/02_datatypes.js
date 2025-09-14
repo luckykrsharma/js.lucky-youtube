1be7d5beadffb9bbb51a61dcbe9f60abdce5cf21
lucky kumar <154545187+luckykrsharma@users.noreply.github.com>
2025-09-14 20:14:39 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treate all js code as newe version
+//alert(3+3) comes error as we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("lucky")
+
+let name = "lucky"
+let age = 20
+let islogedin = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standallone value
+// undefined =>
+    // symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null)// object
+
