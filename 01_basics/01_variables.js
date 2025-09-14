5afcddb3b84643736ce1a77bbda602eb5b9b9d07
lucky kumar <154545187+luckykrsharma@users.noreply.github.com>
2025-09-14 18:11:35 +0000

    variables basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const acciuntId= 144553
+let accountEMAIL= "luckykumarhsarma022@gmail.com"
+var accountpassword ="212289"
+accountcity = "japur"
+let accountstate;
+
+
+// acciuntId =2 not allowed
+accountEMAIL="htc@gmail.com"
+accountcity ="bengluru"
+console.log(acciuntId);
+console.table([acciuntId,accountEMAIL,accountcity,accountstate,accountpassword])
+
+// prefer not to use var because of issue in block scope and functional scope
