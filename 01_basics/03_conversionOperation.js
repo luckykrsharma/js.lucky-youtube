c2eb84d8935ea4e5cc56bb92e934caa198b2aeb7
lucky kumar <154545187+luckykrsharma@users.noreply.github.com>
2025-09-14 20:56:30 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber =Number(score);
+
+// "33" => 33
+// "33abc" => Nan
+//  true => 1; false => 0
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 => false
+//  "lucky" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)// string ayega
+
+// *********************** operation *******************
+ let value =3
+ let negValue = -value
+ console.log(negValue) // -3
+
+ let ste1 = "hello"
+ let ste2 = "lucky"
+ console.log(ste1 +ste2)
+ 
