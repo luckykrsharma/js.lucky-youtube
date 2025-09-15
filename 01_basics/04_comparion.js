9012dc6f0f1eb66e5fa05da6ebe985f81375b1fc
lucky kumar <154545187+luckykrsharma@users.noreply.github.com>
2025-09-15 20:21:15 +0000

    comparison

---
+++ 01_basics/04_comparion.js
@@ -0,0 +1,11 @@
+// console.log(2>1);
+// console.log(2>=1);//examples
+console.log("2">1);//true
+console.log("02">1);// true ayega js comvert karke compare karega
+// === this is strict check isme bina convert ke check karega datatype v check hoga
+
+console.log("2"===2);// false
+
+
+
+
