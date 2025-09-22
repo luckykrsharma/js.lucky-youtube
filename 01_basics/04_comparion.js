3fccb8ffca7f1e1b398b0ac9adc7445939412579
lucky kumar <154545187+luckykrsharma@users.noreply.github.com>
2025-09-22 14:11:30 +0000

    comparition and diffrence

--- 01_basics/04_comparion.js
+++ 01_basics/04_comparion.js
@@ -6,6 +6,17 @@ console.log("02">1);// true ayega js comvert karke compare karega
 
 console.log("2"===2);// false
 
+console.log(null>0); // false
+console.log(null== 0);// false
+console.log(null>=0); // true why? the null is converted to zero by js
 
+/* Key Point:
 
+Comparisons (>, <, >=, <=) convert null to a number (0).
+
+Equality (==) has special rules: null is only equal to undefined and nothing else.
+*/
+
+// === 
+console.log("2" === 2);// triple equal changes the data type then check the condition
 
