cfe19fbac6153256cbbe15cba5764c22087c569a
lucky kumar <154545187+luckykrsharma@users.noreply.github.com>
2025-09-22 14:45:00 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,19 @@
+// javascript is dynamic type language
+
+//primitive and non primitve this type is done n the basis of how data is stored in the memory and fetched from there
+
+// primitve are of 7 types
+// String, Number, boolean, null, undefined, symbol, Bigint
+  
+// refrence (non primitive)
+
+// Array, Object, Function
+let myobj = {
+    name: "lucky",
+    age : 22,
+}
+ const heros = [" saktiman", "iron man", " captain america"]
+
+ const myfunction = function(){
+    console.log("hello world");
+ }
