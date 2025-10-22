79857b94f989962ac8a2191d47d56f066155052c
lucky kumar <154545187+luckykrsharma@users.noreply.github.com>
2025-10-22 20:59:47 +0000

    object basics

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,14 @@
+// singleton
+// object literals
+
+const jsuser ={
+    name: "lucky",
+    "fullname":"lu cky sharma",
+    age:21,
+    location:"bhopal"
+
+}
+
+console.log(jsuser.name)
+console.log(jsuser[fullname])
+// we cannot access the value of full name with dot oprator like name 
